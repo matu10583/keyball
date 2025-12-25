fad7a963f3610f2c7320db831b2461d33292663a
matu10583 <oka0121masa@gmail.com>
2025-12-25 11:28:49 +0900

    combo

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_via/keymap.c
@@ -19,10 +19,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 #include "quantum.h"
+#ifdef COMBO_ENABLE
+enum combos{
+  KL_LCLICK,
+  LSCLN_RCLICK,
+  
+};
+uint16_t const QK_COMBO_KL_V[] = {KC_K, KC_L, COMBO_END};
+uint16_t const QK_COMBO_LSCLN_R[] = {KC_L, KC_SCLN, COMBO_END};
+combo_t key_combos[] = {
+  [KL_LCLICK] = COMBO(QK_COMBO_KL_V, KC_MS_BTN1),
+  [LSCLN_RCLICK] = COMBO(QK_COMBO_LSCLN_R, KC_MS_BTN2)
+};
+#endif
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // keymap for default
+  // keymap for default (VIA)
   [0] = LAYOUT_universal(
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
